78380a2b45290d34133a08dc04f687337703e69c
em0ji <rowi.xyz@gmail.com>
2021-10-05 17:06:04 -0400

    from scapi.layers import http. Move through the layers.

--- packet_sniffer/packet_sniffer.py
+++ packet_sniffer/packet_sniffer.py
@@ -8,6 +8,7 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 sniff("wlan0")
