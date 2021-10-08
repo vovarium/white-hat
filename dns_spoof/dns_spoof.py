73a1bf2b6058416c413e06c4f8c13f9cd318f06a
em0ji <rowi.xyz@gmail.com>
2021-10-08 13:20:36 +0300

    We ping the site ping -c 1 name.site Getting an answer with multiple layers

--- dns_spoof/dns_spoof.py
+++ dns_spoof/dns_spoof.py
@@ -4,7 +4,8 @@ import scapy.all as scapy
 
 def process_packet(packet):
 	scapy_packet = scapy.IP(packet.get_payload())
-	print(scapy_packet.show())
+	if scapy_packet.haslayer(scapy.DNSRR):
+		print(scapy_packet.show())
 	packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
